c0c3def10f76440d0a4662c19c04830680c7230d
ZeroCod <126008647+Thecod0@users.noreply.github.com>
2025-06-14 00:50:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
 
-// Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
-
-bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+const bot = new Eris(process.env.token, {
+  status: "dnd" 
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot.on("error", (err) => {
+  console.error(err);
+});
+
+bot.connect();
