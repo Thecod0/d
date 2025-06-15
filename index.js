81ae2b97fa0c6188c671a3b5cd25944793b5e4d3
ZeroCod <126008647+Thecod0@users.noreply.github.com>
2025-06-15 22:51:22 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const Eris = require("eris");
 
-const bot = new Eris(process.env.token, {
-  status: "dnd" 
-});
+const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
-  console.error(err);
+  console.error(err); // or your preferred logger
 });
 
 bot.connect();
